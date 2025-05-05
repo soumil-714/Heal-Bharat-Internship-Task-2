81174a91d2b8fd8765733ba332290ade04f6a471
soumil-714 <soumil0774@gmail.com>
2025-05-05 11:54:48 +0530

    added javascript file

---
+++ script.js
@@ -0,0 +1,3 @@
+ScrollReveal().reveal('#hero', { duration: 1000, origin: 'top', distance: '30px' });
+ScrollReveal().reveal('#experience', { duration: 1000, origin: 'left', distance: '30px', delay: 200 });
+ScrollReveal().reveal('#projects', { duration: 1000, origin: 'bottom', distance: '30px', delay: 400 });
